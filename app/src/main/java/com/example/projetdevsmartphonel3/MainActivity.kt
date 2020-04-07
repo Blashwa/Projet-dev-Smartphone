40d84ef0b5e07f7d5b5da067d56bd13d39e689ec
Valentin Le Gal <contact@valentinlegal.fr>
2020-04-07 16:50:49 +0200

    Traduction des données de l'accéléromètre

--- app/src/main/java/com/example/projetdevsmartphonel3/MainActivity.kt
+++ app/src/main/java/com/example/projetdevsmartphonel3/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projetdevsmartphonel3
 
 import android.content.Context
+import android.content.res.Configuration
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -52,7 +53,80 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null) {
-            Log.v("INFOS", "X = " + event.values[0] + "\tY = " + event.values[1] + "\tZ = " + event.values[2])
+            //Log.v("INFOS", "X = " + event.values[0] + "\tY = " + event.values[1] + "\tZ = " + event.values[2])
+            translateAccelerometer(event.values[0], event.values[1], event.values[2])
         }
+    }
+
+    fun translateAccelerometer(valueX: Float, valueY: Float, valueZ: Float) {
+
+        var sensibilite = 5
+
+        var avant = false
+        var arriere = false
+        var gauche = false
+        var droite = false
+
+        // On traduit la valeur Z
+        if (valueZ > sensibilite) {
+            avant = true
+            arriere = false
+        } else if (valueZ < -sensibilite) {
+            arriere = true
+            avant = false
+        } else {
+            avant = false
+            arriere = false
+        }
+
+        // Le telephone est oriente en mode portrait
+        if(resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT){
+            // On traduit la valeur X
+            if (valueX > sensibilite) {
+                gauche = true
+                droite = false
+            } else if (valueX < -sensibilite) {
+                droite = true
+                gauche = false
+            } else {
+                gauche = false
+                droite = false
+            }
+        }
+
+        // Le telephone est oriente en mode paysage
+        if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            // On traduit la valeur Y
+            if (valueY > sensibilite) {
+                gauche = true
+                droite = false
+            } else if (valueY < -sensibilite) {
+                droite = true
+                gauche = false
+            } else {
+                gauche = false
+                droite = false
+            }
+        }
+
+        // On affiche la traduction dans le terminal
+        if (avant && gauche) {
+            Log.v("INFOS", "AVANT GAUCHE")
+        } else if (avant && droite) {
+            Log.v("INFOS", "AVANT DROITE")
+        } else if (avant) {
+            Log.v("INFOS", "AVANT")
+        } else if (arriere && gauche) {
+            Log.v("INFOS", "ARRIERE GAUCHE")
+        } else if (arriere && droite) {
+            Log.v("INFOS", "ARRIERE DROITE")
+        } else if (arriere) {
+            Log.v("INFOS", "ARRIERE")
+        } else if (gauche) {
+            Log.v("INFOS", "GAUCHE")
+        } else if (droite) {
+            Log.v("INFOS", "DROITE")
+        }
+
     }
 }
